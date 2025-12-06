cd09ee49905331498628c4d0b156e5597b4e52c8
ylh551400 <yaolvhuan0627@gmail.com>
2025-12-05 21:20:54 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,16 +1,1 @@
-# DO NOT PUT API KEY IN CODE
-# Use environment variables instead
-
-analyzer = TinderRAGAnalyzer()   
-question = "What are the biggest complaints in the last 12 months?"
-
-result = analyzer.ask(
-    query=question,
-    n_reviews=15,
-    filters={'is_negative': True}
-)
-
-print("=" * 60)
-print("Analysis Result:")
-print("=" * 60)
-print(result)
+from .rag_with_claude import TinderRAGAnalyzer
